50c3a7405dd467e42d8188da04a4c6aedd769df5
Andhika Prakasiwi <p.andhika@op.iitg.ac.in>
2025-09-26 10:34:11 +0700

    refactor: enhance button and simplify index structure

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,5 @@
-import Button from "@/components/button";
-import CustomComponent from "@/components/custom-component";
-import { View, Text } from "react-native";
+import StyleComponent from "@/components/style-component";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <CustomComponent name="Custom" />
-      <Button onPress={() => alert("pressed!")}>Press me</Button>
-    </View>
-  );
+  return <StyleComponent />;
 }
--- components/button.tsx
+++ components/button.tsx
@@ -1,24 +1,101 @@
 import { ReactNode } from "react";
-import { Pressable, Text } from "react-native";
+import {
+  ActivityIndicator,
+  Pressable,
+  Text,
+  TextStyle,
+  ViewStyle,
+} from "react-native";
+
+type Size = "small" | "medium" | "large";
+type Variant = "filled" | "outline" | "ghost";
 
 type Props = {
   onPress?: () => void;
   children?: ReactNode;
-  disable?: boolean;
+  disabled?: boolean;
   loading?: boolean;
-  size?: "small" | "medium" | "large";
+  size?: Size;
+  variant?: Variant;
+  style?: ViewStyle;
+  textStyle?: TextStyle;
 };
 
 export default function Button({
   onPress,
   children,
-  disable,
+  disabled,
   loading,
-  size,
+  size = "small",
+  variant = "filled",
+  style,
+  textStyle,
 }: Props) {
+  const sizeStyle: Record<
+    Size,
+    { height: number; fontSize: number; padding: number }
+  > = {
+    small: { height: 36, fontSize: 14, padding: 12 },
+    medium: { height: 44, fontSize: 16, padding: 16 },
+    large: { height: 55, fontSize: 18, padding: 20 },
+  };
+
+  const getVariantStyle = (): ViewStyle => {
+    switch (variant) {
+      case "filled":
+        return {
+          backgroundColor: "black",
+        };
+      case "outline":
+        return {
+          backgroundColor: "transparent",
+          borderWidth: 1,
+          borderColor: "gray",
+        };
+      default:
+        return {
+          backgroundColor: "transparent",
+        };
+    }
+  };
+
+  const getTextStyle = (): string => {
+    switch (variant) {
+      case "filled":
+        return "white";
+      case "outline":
+        return "balck";
+      default:
+        return "black";
+    }
+  };
+
   return (
-    <Pressable onPress={onPress}>
-      <Text>{children}</Text>
+    <Pressable
+      onPress={onPress}
+      style={[
+        getVariantStyle(),
+        {
+          borderRadius: 12,
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          height: sizeStyle[size].height,
+          padding: sizeStyle[size].padding,
+          opacity: disabled ? 0.5 : 1,
+        },
+        style,
+      ]}
+      disabled={disabled || loading}
+    >
+      <Text
+        style={[
+          { color: getTextStyle(), fontSize: sizeStyle[size].fontSize },
+          textStyle,
+        ]}
+      >
+        {loading ? <ActivityIndicator /> : children}
+      </Text>
     </Pressable>
   );
 }
